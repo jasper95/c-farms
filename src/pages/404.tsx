4595af7a27d64fd1d8da990f7e37d323b8eda358
Jasper Bernales <bernalesjasper@gmail.com>
2022-04-10 07:49:34 +0800

    lint stage testing

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,5 +1,5 @@
 import DashboardLayout from '@/components/layout/dashboard-layout'
 
 export default function Custom404() {
-  return <DashboardLayout>Page Not found</DashboardLayout>
+  return <DashboardLayout>Page Not found Temporary Page</DashboardLayout>
 }
