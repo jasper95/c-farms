88718337e434ca1dd9e6d3bd094a139e3188587a
Jasper Bernales <bernalesjasper@gmail.com>
2022-09-28 13:17:55 +0800

    test

--- src/modules/household/views/household-list.view.tsx
+++ src/modules/household/views/household-list.view.tsx
@@ -4,20 +4,22 @@ import SearchBar from '@/components/search-bar'
 import Link from '@/components/link'
 import DataTable from '@/components/data-table'
 import { useHouseholdList } from '@/modules/household/hooks'
-import getDashboardLayout from '@/components/layout/dashboard.layout'
+import getDashboardLayout, {
+  Dashboard,
+} from '@/components/layout/dashboard.layout'
 
 export function HouseholdListView() {
-  // const {
-  //   columns,
-  //   rows,
-  //   tableDispatch,
-  //   tableState,
-  //   onSearchChanged,
-  //   listResponse,
-  //   totalRows,
-  // } = useHouseholdList()
+  const {
+    columns,
+    rows,
+    tableDispatch,
+    tableState,
+    onSearchChanged,
+    listResponse,
+    totalRows,
+  } = useHouseholdList()
   return (
-    <>
+    <Dashboard>
       <Breadcrumbs crumbs={[{ name: 'Household' }]} />
       <div className="grid grid-cols-12 gap-4 pb-4">
         <div className="col-span-full md:col-span-4 flex items-center">
@@ -27,11 +29,11 @@ export function HouseholdListView() {
             </Button>
           </Link>
         </div>
-        {/* <div className="col-span-full col-start-0 md:col-span-4 md:col-start-9">
+        <div className="col-span-full col-start-0 md:col-span-4 md:col-start-9">
           <SearchBar onChange={onSearchChanged} />
-        </div> */}
+        </div>
       </div>
-      {/* <DataTable
+      <DataTable
         columns={columns}
         tableDispatch={tableDispatch}
         tableState={tableState}
@@ -39,9 +41,7 @@ export function HouseholdListView() {
         rows={rows}
         showPagination
         loading={listResponse.fetching}
-      /> */}
-    </>
+      />
+    </Dashboard>
   )
 }
-
-HouseholdListView.getLayout = getDashboardLayout
