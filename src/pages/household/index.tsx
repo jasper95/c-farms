5c534a805fca6550c602ce676fb7f368bca85ff1
Jasper Bernales <bernalesjasper@gmail.com>
2022-09-28 13:31:05 +0800

    test

--- src/pages/household/index.tsx
+++ src/pages/household/index.tsx
@@ -1,11 +1,3 @@
-import Breadcrumbs from '@/components/breadcrumbs'
-import Button from '@mui/material/Button'
-import SearchBar from '@/components/search-bar'
-import Link from '@/components/link'
-import DataTable from '@/components/data-table'
-import { useHouseholdList } from '@/modules/household/hooks'
-import { Dashboard } from '@/components/layout/dashboard.layout'
+import { HouseholdListView } from '@/modules/household/views'
 
-export default function HouseholdsPage() {
-  return <Dashboard>Test</Dashboard>
-}
+export default HouseholdListView
