912fab563810458c9cdb742106569cf717f99586
Jasper Bernales <bernalesjasper@gmail.com>
2022-09-28 13:24:47 +0800

    test

--- src/pages/household/index.tsx
+++ src/pages/household/index.tsx
@@ -1,1 +1,7 @@
-export { HouseholdListView as default } from '@/modules/household/views'
+// export { HouseholdListView as default } from '@/modules/household/views'
+
+import { Dashboard } from '@/components/layout/dashboard.layout'
+
+export default function HouseholdsPage() {
+  return <Dashboard>Test</Dashboard>
+}
