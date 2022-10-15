d18f8e5fab1e525d11aba14d0c151c088caab814
almamae <maebernales@gmail.com>
2022-10-15 20:15:58 +0800

    fix typescript error

--- src/modules/annual-info/constants/annual-info-list-filters.ts
+++ src/modules/annual-info/constants/annual-info-list-filters.ts
@@ -1,5 +1,6 @@
 import { DataTableFilter } from '@/components/data-table/types'
 import { FieldTypeEnum } from '@/components/list-filter/field-type.enum'
+import * as T from '@/lib/generated/graphql.types'
 
 export const annualInfoListFilters: DataTableFilter<T.AnnualInfo>[] = [
   {
