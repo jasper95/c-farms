7d0e9c34cd71eb4413ff4feac483a5b90e0584e3
almamae <maebernales@gmail.com>
2022-10-23 22:00:05 +0800

    update required fields of household details

--- src/modules/household/constants/other-details-schema.ts
+++ src/modules/household/constants/other-details-schema.ts
@@ -1,18 +1,19 @@
+import { fieldIsRequired } from '@/lib/utils/form-utils'
 import * as yup from 'yup'
 
 export const otherDetailsSchema = yup.object({
   nameOfHouseholdHead: yup.string().default(''),
   relationshipToHouseholdHead: yup.string().default(''),
-  maleCount: yup.number().default(0),
-  femaleCount: yup.number().default(0),
+  maleCount: yup.number().required(fieldIsRequired).default(0),
+  femaleCount: yup.number().required(fieldIsRequired).default(0),
   governmentIdType: yup.string().default(''),
   governmentIdNo: yup.string().default(''),
   personWithDisability: yup.boolean().default(false),
   is4psBeneficiary: yup.boolean().default(false),
   isHouseholdHead: yup.boolean().default(false),
   ipMembership: yup.string().default(''),
-  emergencyContactName: yup.string().default(''),
-  emergencyContactNumber: yup.string().default(''),
+  emergencyContactName: yup.string().required(fieldIsRequired).default(''),
+  emergencyContactNumber: yup.string().required(fieldIsRequired).default(''),
 })
 
 export type IOtherDetailsSchema = yup.InferType<typeof otherDetailsSchema>
