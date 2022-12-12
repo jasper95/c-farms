3e0fb144e28effcaa2ac3292e27402d64c417e3a
almamae <maebernales@gmail.com>
2022-12-12 10:26:46 +0800

    update commodity produce form

--- src/modules/commodity-produce/constants/commodity-produce-schema.ts
+++ src/modules/commodity-produce/constants/commodity-produce-schema.ts
@@ -4,9 +4,15 @@ export const commodityProduceSchema = yup.object({
   year: yup.number().default(new Date().getFullYear()),
   farmId: yup.string().label('Farm').required().default(''),
   commodityId: yup.string().label('Commodity').required().default(''),
+  farmSize: yup.number().default(0),
   produce: yup.number().default(0),
   organicPractitioner: yup.boolean().default(false),
-  areaUsed: yup.number().default(0),
+  areaUsed: yup
+    .number()
+    .default(0)
+    .positive()
+    .moreThan(0, `Area must be more than 0.`)
+    .max(yup.ref('farmSize'), 'You have exceeded the farm size.'),
   produceInUnit: yup.number().default(0),
   unit: yup.string().default('MT'),
 })
